b136918898065cdb3e79ac1734ec4c45963257fc
Noah Visser <ikbenbeter0508@gmail.com>
2025-10-03 09:54:55 +0200

    Update Tafel oefenen.py

--- Tafel oefenen.py
+++ Tafel oefenen.py
@@ -1,10 +1,15 @@
 import random
 
-score = 0
+tafel = int(input("Welke tafel wil je oefenen? (bijv. 3): "))
 
 while True:
-    input("Druk op Enter om te gooien...")
-    worp = random.randint(1, 6)
-    print(f"Je gooide: {worp}")
-    score += worp
-    print(f"Totaalscore: {score}\n")
+    getal = random.randint(1, 10)
+    antwoord = input(f"Wat is {tafel} x {getal}? ")
+    if antwoord == "stop":
+        print("Gestopt met oefenen.")
+        break
+    if antwoord.isdigit() and int(antwoord) == tafel * getal:
+        print("Goed!\n")
+    else:
+        print(f"Fout. Het juiste antwoord is {tafel * getal}.\n")
+
